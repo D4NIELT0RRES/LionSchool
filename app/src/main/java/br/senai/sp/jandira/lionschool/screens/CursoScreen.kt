fc23c21377a15b97681d95b905d4dace44070504
danieltorres <161612167+D4NIELT0RRES@users.noreply.github.com>
2025-04-15 12:24:30 -0300

    Iniciando Lion School

---
+++ app/src/main/java/br/senai/sp/jandira/lionschool/screens/CursoScreen.kt
@@ -0,0 +1,57 @@
+package br.senai.sp.jandira.lionschool.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TelaCurso(){}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@Preview(showSystemUi = true)
+@Composable
+private fun TelaCursoPreview(){
+    TelaCurso()
+}
